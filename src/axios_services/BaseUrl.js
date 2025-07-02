5c89dbfb804809d2a4eafe27173855756892e108
SKNGroup <Karimefendiyev1@gmail.com>
2025-07-02 17:28:43 +0400

    Update BaseUrl.js

--- src/axios_services/BaseUrl.js
+++ src/axios_services/BaseUrl.js
@@ -1,7 +1,7 @@
 
 
 // export const baseUrl = "https://skngroup-gyh0a6haaweeckaw.canadacentral-01.azurewebsites.net/api/";
-export const baseUrl = "https://skngroup-app-fkhhaca7fvcce0cx.canadacentral-01.azurewebsites.net/api/";
+export const baseUrl = "skngroup-app-h4gfa9b6c8g3bmes.canadacentral-01.azurewebsites.net/api/";
 export const baseImgUrl = "https://skngroupstoragefiles.blob.core.windows.net/";  
 
 
